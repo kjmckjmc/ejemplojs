355f2ad0ce18190566dff873fc263eab13706012
kjmckjmc <kellyjohannamunera2014@gmail.com>
2020-09-02 21:15:08 -0500

    agrego ejemplo1 al folder

---
+++ ejemplo1/js/variables.js
@@ -0,0 +1,12 @@
+//scope 
+//variable global: se puede usar en cualquier parte dle proyecto
+//variable local, dentro de una funcion, que solo le interesa a esa funcion usar esa variable
+
+// let se usa para variables globales 
+let nombre = 'fernando'
+//let edad = 35
+estatura = 1.71
+const fechaEspecial = "20-10-2020"
+const estadoCasado = true
+
+document.write(`tu nombre es : ${nombre} y tu estatura es ${estatura}`)
